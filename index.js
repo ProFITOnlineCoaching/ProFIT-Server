3a17aa65374a9b069357d8a349b480ee149ac202
ProFITOnlineCoaching <profitteam10@gmail.com>
2024-03-26 01:52:50 +0200

    Fixed API Names

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import { globalErrorHandling } from "./src/middlewares/GlobalErrorHandling.js";
 import { AppError } from "./src/utils/AppError.js";
 
 import cors from 'cors';
-import tranieeAuthRouter from './src/modules/TraineeMobile/Auth/auth.routes.js';
+import traineeAuthRouter from './src/modules/TraineeMobile/Auth/auth.routes.js';
 import trainerAuthRoutes from "./src/modules/TrainerDashboard/Auth/auth.routes.js";
 import adminAuthRoutes from "./src/modules/AdminDashboard/Auth/auth.routes.js";
 import adminControllerRoutes from "./src/modules/AdminDashboard/admin.routes.js";
-import tranieeExploreRouter from "./src/modules/TraineeMobile/Explore/explore.routes.js";
+import traineeExploreRouter from "./src/modules/TraineeMobile/Explore/explore.routes.js";
 const app = express();
 const port = 4000;
 app.use(cors());
@@ -23,8 +23,8 @@ app.use("/api/v1/dashboard/auth", adminAuthRoutes);
 
 
 
-app.use("/api/v1/mobile/traniee/auth", tranieeAuthRouter);
-app.use("/api/v1/mobile/traniee/explore", tranieeExploreRouter);
+app.use("/api/v1/mobile/trainee/auth", traineeAuthRouter);
+app.use("/api/v1/mobile/traniee/explore", traineeExploreRouter);
 app.use("/api/v1/dashboard/tranier/auth", trainerAuthRoutes);
 app.use("/api/v1/dashboard/admin/auth", adminAuthRoutes);
 app.use("/api/v1/dashboard/admin", adminControllerRoutes);
